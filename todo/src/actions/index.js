4877dfeb70690f2682d4918c022f230f208ceb88
Michael Williams <manicdesigner@gmail.com>
2019-07-11 17:28:26 -0400

    fix(actions/index) + alter function name too addTodo from ADD_TODO

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,6 +1,6 @@
 export const ADD_TODOS = 'ADD_TODOS';
 
-export const ADD_TODO = (todo, completed) => {
+export const addTodo = (todo, completed) => {
     return {
         type: ADD_TODOS,
         payload: {

ae947ccbe25523a22afdf0775a8e0112feefec73
Michael Williams <manicdesigner@gmail.com>
2019-07-12 12:28:35 -0400

    feat(reducer) + built out reducer function + initialState added

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,3 +1,14 @@
-export default () => {
+import { ADD_TODO } from '../actions/index';
 
-}
+const initialState = {
+  value: '',
+  completed: false,
+  todos: []
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};

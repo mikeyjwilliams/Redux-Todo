57fee6dab55ad567b3bf75e1a62ea6bf904aa253
Michael Williams <manicdesigner@gmail.com>
2019-07-12 13:20:13 -0400

    feat(actions/index) + return statement with type, payload { todo, completed}

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,6 +1,14 @@
 export const ADD_TODO = 'ADD_TODO';
 export const COMEPLETED_TODO = 'COMPLETED_TODO';
 
-export const addTodo = (todo, completed) => {};
+export const addTodo = (todo, completed) => {
+  return {
+    type: ADD_TODO,
+    payload: {
+      todo,
+      completed
+    }
+  };
+};
 
 export const todoCompleteToggle = (todo, completed) => {};

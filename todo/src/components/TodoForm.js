cb8105899e8aa74cf7d1447eabb6e466972dfb60
Michael Williams <manicdesigner@gmail.com>
2019-07-12 13:08:37 -0400

    feat(TodoForm) + state for form + changeHandler handle name and value + addItem on submit

--- todo/src/components/TodoForm.js
+++ todo/src/components/TodoForm.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 
 class TodoForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: '',
+      completed: false,
+      todos: []
+    };
+  }
+
+  changeHandler = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  addItem = e => {
+    e.preventDefault();
+
+    const { todo, completed } = this.state;
+    //this.props.addToItemList(todo, completed);
+    console.log('addItem', todo);
+    this.setState({
+      todo: ''
+    });
+  };
+
   render() {
+    const { todo } = this.state;
     return (
       <div>
-        <form>
+        <form onSubmit={this.addItem}>
           <input
             type="text"
-            value="todo"
+            value={todo}
             onChange={this.changeHandler}
-            name="todos"
+            name="todo"
           />
           <button type="submit">add to list</button>
         </form>

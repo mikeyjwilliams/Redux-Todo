b5adf5660f492bd43cd9e0236a5de4a63e904d0b
Michael Williams <manicdesigner@gmail.com>
2019-07-13 12:57:34 -0400

    test(App.js) + TodoList component to test list items

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoForm from './components/TodoForm';
 import './App.css';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <TodoForm />
+      <TodoList />
     </div>
   );
 }

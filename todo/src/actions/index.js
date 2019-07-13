bc96f2e53bc50c6e9c2be7fd2e8b4f46c25a114d
Michael Williams <manicdesigner@gmail.com>
2019-07-13 12:56:37 -0400

    feat(actions) + console.log payload

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,7 +1,8 @@
 export const ADD_TODO = 'ADD_TODO';
-export const COMEPLETED_TODO = 'COMPLETED_TODO';
+export const COMPLETED_TODO = 'COMPLETED_TODO';
 
 export const addTodo = (todo, completed) => {
+  console.log('payload', todo);
   return {
     type: ADD_TODO,
     payload: {

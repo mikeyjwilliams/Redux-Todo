db7e8de6784a1b4c882ecbda1e38d9a6d8bd6b4e
Michael Williams <manicdesigner@gmail.com>
2019-07-13 12:55:16 -0400

    test(reducer) +add test console.logs

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,13 +1,27 @@
 import { ADD_TODO } from '../actions/index';
 
 const initialState = {
-  value: '',
+  todo: '',
   completed: false,
   todos: []
 };
 
 export default (state = initialState, action) => {
+  console.log('state', state);
   switch (action.type) {
+    case ADD_TODO:
+      // const { todo } = this.state;
+      const { anotherTodo } = action.payload;
+      // //const newTodo = state.todos.concat([newState]);
+      // console.log(anotherTodo);
+      console.log('firing');
+      console.log('payload', anotherTodo);
+      console.log('action.type', action.type);
+      break;
+    // return {
+    //   ...state,
+    //   todos: newTodo
+    // };
     default:
       return state;
   }

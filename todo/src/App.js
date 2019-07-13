fddaf53baf1e92722b49a50fe1f439f104305b4a
Michael Williams <manicdesigner@gmail.com>
2019-07-13 18:30:26 -0400

    remove(App.js) - remove component TodoForm

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import TodoForm from './components/TodoForm';
 import './App.css';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
-      <TodoForm />
       <TodoList />
     </div>
   );

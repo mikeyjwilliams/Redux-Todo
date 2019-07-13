db9a216a59bc0d1386e1800009a2407f2ed9c113
Michael Williams <manicdesigner@gmail.com>
2019-07-13 13:55:33 -0400

    feat(action) + GET_TODOS + getTodos handler added

--- todo/src/actions/index.js
+++ todo/src/actions/index.js
@@ -1,10 +1,18 @@
 export const ADD_TODO = 'ADD_TODO';
+export const GET_TODOS = 'GET_TODOS';
 export const COMPLETED_TODO = 'COMPLETED_TODO';
 
 export const addTodo = todo => {
   return {
     type: ADD_TODO,
     payload: todo
+  };
+};
+
+export const getTodos = todos => {
+  return {
+    type: GET_TODOS,
+    payload: todos
   };
 };
 

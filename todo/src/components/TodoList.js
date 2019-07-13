450e27aee74aee4b0b060817ca1007273c4b74a4
Michael Williams <manicdesigner@gmail.com>
2019-07-13 18:16:24 -0400

    feat(TodoList) + map todos prop with a key style if complete else null show todo - removed all state but what was needed for form + test for state

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,13 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { addTodo, getTodos } from '../actions/index';
 
-function TodoList(props) {
-  return (
-    <div>
-      <ul>
-        <li>todo items</li>
-      </ul>
-    </div>
-  );
+class TodoList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    };
+  }
+
+  changeHandler = e => {
+    e.preventDefault();
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  addItem = e => {
+    e.preventDefault();
+
+    const { input } = this.state;
+    let todo = input;
+    this.props.addTodo(todo);
+    this.setState({
+      input: ''
+    });
+  };
+
+  render() {
+    const { todos } = this.props;
+    return (
+      <div>
+        <form onSubmit={this.addItem}>
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={this.changeHandler}
+            name="input"
+          />
+          <button type="submit">add to list</button>
+        </form>
+        {todos.map((todo, i) => {
+          return (
+            <p
+              key={i}
+              style={
+                todo.complete
+                  ? { textDecoration: 'line-through', color: 'red' }
+                  : null
+              }
+            >
+              {todo.todo}
+            </p>
+          );
+        })}
+      </div>
+    );
+  }
 }
-export default connect()(TodoList);
+
+const mapStateToProps = state => {
+  console.log('mapstatetoprops todolist', state);
+  return { todos: state.todos };
+};
+
+const mapDispatchToProps = {
+  addTodo: addTodo
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);

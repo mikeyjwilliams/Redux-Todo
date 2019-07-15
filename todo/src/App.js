088ee9d21d9918d73d30e992d765a555b480c786
Michael Williams <manicdesigner@gmail.com>
2019-07-15 15:22:47 -0400

    feat(App.js) + headers + TodoList still there future TodoList component coming

--- todo/src/App.js
+++ todo/src/App.js
@@ -5,7 +5,9 @@ import TodoList from './components/TodoList';
 function App() {
   return (
     <div className="App">
+      <h2>The List</h2>
       <TodoList />
+      <h3>Todo List:</h3>
     </div>
   );
 }

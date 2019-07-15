57b31479b890c879be26d8b0fb91485694417678
Michael Williams <manicdesigner@gmail.com>
2019-07-15 15:34:03 -0400

    feat(TodoList) + mapStateToProps + connect react-redux

--- todo/src/components/TodoList.js
+++ todo/src/components/TodoList.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-function TodoList(props)  {
-return (
-    {
-        todos.map((todo, i) => {
-            return (
-                <p
-                    onClick={this.toggleComplete}
-                    key={i}
-                    style={
-                        todo.complete
-                            ? { textDecoration: 'line-through', color: 'red' }
-                            : null
-                    }
-                >
-                    {todo.todo}
-                </p>
-            );
-        })
-    }
- )
+function TodoList(props) {
+  const { todos } = props;
+  return (
+    <div>
+      {todos.map((todo, i) => {
+        return (
+          <p
+            // onClick={this.toggleComplete}
+            key={i}
+            style={
+              todo.complete
+                ? { textDecoration: 'line-through', color: 'red' }
+                : null
+            }
+          >
+            {todo.todo}
+          </p>
+        );
+      })}
+    </div>
+  );
 }
-export default TodoList;
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
+};
+
+export default connect(mapStateToProps)(TodoList);

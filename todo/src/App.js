47a88c511334c78fa45d19e026ae22a51f59183a
Michael Williams <manicdesigner@gmail.com>
2019-07-15 15:33:10 -0400

    feat(App.js) + TodoList component added

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
 import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
   return (
     <div className="App">
       <h2>The List</h2>
-      <TodoList />
+      <TodoForm />
       <h3>Todo List:</h3>
+      <TodoList />
     </div>
   );
 }

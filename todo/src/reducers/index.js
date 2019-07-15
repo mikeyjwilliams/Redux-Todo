8a88ceccaa1001bde1686b9fab014afcde5a8149
Michael Williams <manicdesigner@gmail.com>
2019-07-15 14:27:31 -0400

    feat(reducer) + ADD_TODO id string + theTodo object + concat new todo with original todos return new state + TOGGLE_COMPLETE console.log payload array too var find the object I want with .find console log it

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -9,13 +9,34 @@ export default (state = initialState, action) => {
     case ADD_TODO:
       let { todos } = state;
       const todo = action.payload;
-      const newTodos = [...todos, todo];
+
+      let id = Math.random()
+        .toString(36)
+        .substring(2);
+      let theTodo = { todo: todo, id: id, complete: false };
+
+      const newTodos = todos.concat([theTodo]);
+      console.log('ADD_TODO', newTodos);
       return { todos: newTodos };
 
     case TOGGLE_COMPLETE:
-      // const id = state;
-      const { todoList } = action.payload;
-      console.log(todoList);
+      console.log('TOGGLE_COMPLETE payload', action.payload);
+      // const todoId = ;
+      // console.log('state id', todoId);
+      const [payload] = action.payload;
+      console.log(payload);
+      const todoItemList = [payload];
+      // console.log('TOGGLE_COMPLETE', todoItem, todoId, complete);
+
+      const toggle = todoItemList.find(item => item.id === payload.id);
+      console.log(toggle);
+
+      // if (todoList.id === todoItem.id) {
+      //   todoList.complete = !todoList.complete;
+      // }
+      // console.log('TOGGGLE_COMPLETE', todoList);
+      // const newTodoList = [...todoList, todoList];
+      // return { todos: newTodoList };
       break;
     default:
       return state;
